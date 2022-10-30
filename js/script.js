522898629345a30a12f3c46ef06fe6fc736f8c7e
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2022-10-30 16:55:54 -0300

    Finalizando o script da tarefa

--- js/script.js
+++ js/script.js
@@ -9,12 +9,46 @@ function escrever() {
 }
 
 /*document.getElementById("password").value =document.getElementById(this).value;*/
-
-function num(x){
-  let senha = [6]
-  senha = document.getElementById('visor').innerHTML;
-  document.getElementById('visor').innerHTML = senha + x;
 /*
-for(let i=0; i> senha.length;i++){
+function num(x) {
+  let senha = document.getElementById("visor").innerHTML
+  // let tam = senha.length - 75;
+  let tam = senha.length - 75
+
+  do {
+    document.getElementById("visor").innerHTML = senha + x
+  } while (tam > 6)
+
+  console.log(senha.length - 75)
+
 }*/
+
+function num(x) {
+  let senha = document.getElementById("visor").innerHTML
+  document.getElementById("visor").innerHTML = senha + x
+}
+
+function tam() {
+  let tamanho =
+    document.getElementById(
+      "visor"
+    ).innerText /*para puxar corretamente o tamanho do texto tem que usar o innerText */
+  console.log(tamanho.length)
+  if (tamanho.length > 6) {
+    document.getElementById("visor").innerHTML = tamanho.substring(
+      0,
+      tamanho.length - 1
+    )
+  }
+}
+
+function limparTudo() {
+  document.getElementById("visor").innerHTML = ""
+}
+function limpar() {
+  let senha = document.getElementById("visor").innerHTML
+  document.getElementById("visor").innerHTML = senha.substring(
+    0,
+    senha.length - 1
+  )
 }
